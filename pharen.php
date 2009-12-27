572abef6d75d990cf69e954676fb89c44d141885
Tamreen <historium@gmail.com>
2009-12-27 03:21:37 -0500

    Created working lexer, lexes parens, names, strings, and numbers as their own tokens

--- pharen.php
+++ pharen.php
@@ -5,25 +5,32 @@ class Token{
     public function __construct($value=null){
         $this->value = $value;
     }
+
+    public function append($ch){
+        $this->value .= $ch;
+    }
 }
 
-class OpenParenToken{
+class OpenParenToken extends Token{
 }
 
-class CloseParenToken{
+class CloseParenToken extends Token{
 }
 
-class StringToken{
+class NumberToken extends Token{
 }
 
-class NameToken{
+class StringToken extends Token{
+}
+
+class NameToken extends Token{
 }
 
 class Lexer{
 	private $code;
     private $char;
     private $tok;
-	private $state = "";
+	private $state = "new-expression";
     private $toks = array();
     private $escaping = false;
     private $i=0;
@@ -34,19 +41,61 @@ class Lexer{
 
     public function lex(){
         for($this->i=0;$this->i<strlen($this->code);$this->i++){
-
+            $this->get_char();
+            $this->lex_char();
+            if($this->tok !== $this->toks[sizeof($this->toks)-1]){
+                $this->toks[] = $this->tok;
+            }
+        }
+        return $this->toks;
     }
 
     public function get_char(){
-        $this->char = $code[$i++];
+        $this->char = $this->code[$this->i];
     }
 
     public function lex_char(){
-        if($this->char == "("){
-            $this->tok = new OpenParenToken;
-        }else if($this->char == ")"){
-            $this->tok = new CloseParenToken;
-        }else if(
+        if($this->state == "string"){
+            if($this->char == '"' && !$this->escaping){
+                $this->state = "new-expression";
+            }else if($this->char == "\\" && !$this->escaping){
+                $this->escaping = True;
+            }else if($this->escaping){
+                $this->escaping = False;
+            }else{
+                $this->tok->append($this->char);
+            }
+        }else if($this->state == "append"){
+            if(trim($this->char) === ""){
+                $this->state = "new-expression";
+            }else if($this->char == ")"){
+                $this->tok = new CloseParenToken;
+                $this->state = "new-expression";
+            }else{
+                $this->tok->append($this->char);
+            }
+        }else if($this->state == "new-expression"){
+            // For function calls, a function name can itself be a sexpr that returns a function name
+            if($this->char == "("){
+                $this->tok = new OpenParenToken;
+            }else if($this->char == ")"){
+                // Allow for empty set of parens
+                $this->tok = new CloseParenToken;
+            }else if($this->char == '"'){
+                $this->tok = new StringToken;
+                $this->state = "string";
+            }else if(is_numeric($this->char)){
+                $this->tok = new NumberToken($this->char);
+                $this->state = "append";
+            }else if(trim($this->char) !== ""){
+                $this->tok = new NameToken($this->char);
+                $this->state = "append";
+            }
+        }            
     }
-
 }
+
+$code = file_get_contents("simple.phn");
+$code = trim($code);
+$lexer = new Lexer($code);
+print_r($lexer->lex());
