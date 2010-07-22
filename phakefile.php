c405bdce64b49e63c3fb234cab21f961f0234156
Tamreen <historium@gmail.com>
2010-07-22 00:54:31 -0400

    Remove compilation of lang.phn and phake.phn, since those are compiled inside pharen.php and while running phake respectively.

--- phakefile.php
+++ phakefile.php
@@ -1,4 +1,3 @@
-#! /usr/bin/env php
 <?php
 require_once('/Applications/MAMP/htdocs/pharen/lang.php');
 Lexical::$scopes['phakefile'] = array();
@@ -8,9 +7,7 @@ function build(){
 	$__scope_id = Lexical::init_closure("phakefile", 32);
 	$body =& Lexical::get_lexical_binding('phakefile', 33, '$body', isset($__closure_id)?$__closure_id:0);;
 print(("Running " . "build" . ": " . "Compiling all project files written in Pharen" . "\n"));
-compile_dir(project_path("/lib/"));
-compile_with_flag("no-import-lang", "lang.phn");
-return compile_with_flag("executable", project_path("/lib/phake/phake.phn"));
+return compile_dir(project_path("/lib/"));
 }
 
 ;
