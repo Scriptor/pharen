255bf96ff44bc8fe35f89637a375b1dd9e16d70b
Tamreen <historium@gmail.com>
2010-08-07 12:57:51 -0400

    Add a compile-except function to the phake library that compiles all files except a certain one. Also, compile-dir now lets you specify what compile function to use, with compile-file being the default one.

--- lib/phake/phake.php
+++ lib/phake/phake.php
@@ -1,3 +1,4 @@
+#! /usr/bin/env php
 <?php
 require_once('/Applications/MAMP/htdocs/pharen/lang.php');
 Lexical::$scopes['phake'] = array();
@@ -29,42 +30,58 @@ function compile_with_flag($flag, $file){
 	return unset_flag($flag);
 }
 
-function is_phn($f){
+function compile_except($except, $file, $output_dir=NULL){
 
-$__listAcessTmpVar2 = pathinfo($f);
-$__listAcessTmpVar3 = pathinfo($f);
-	return (isset($__listAcessTmpVar2["extension"]) and ($__listAcessTmpVar3["extension"] == "phn"));
+	print(("Except: " . $except . " " . $file . "\n"));
+
+$__condtmpvar4 = Null;
+if(($except !== $file)){
+	$__condtmpvar4 = compile_file($file);
+}
+else{
+$__condtmpvar4 = FALSE;
+}
+	return $__condtmpvar4;
 }
 
-function phake__lambdafunc1($f, $__closure_id){
-	$__scope_id = Lexical::init_closure("phake", 29);
-	$dir =& Lexical::get_lexical_binding('phake', 28, '$dir', isset($__closure_id)?$__closure_id:0);;
+function is_phn($f){
+
+$__listAcessTmpVar0 = pathinfo($f);
+$__listAcessTmpVar1 = pathinfo($f);
+	return (isset($__listAcessTmpVar0["extension"]) and ($__listAcessTmpVar1["extension"] == "phn"));
+}
+
+function phake__lambdafunc3($f, $__closure_id){
+	$__scope_id = Lexical::init_closure("phake", 31);
+	$dir =& Lexical::get_lexical_binding('phake', 30, '$dir', isset($__closure_id)?$__closure_id:0);;
+	$compile_func =& Lexical::get_lexical_binding('phake', 30, '$compile_func', isset($__closure_id)?$__closure_id:0);;
 	$file = path_join($dir, $f);
-	Lexical::bind_lexing("phake", 29, '$file', $file);
+	Lexical::bind_lexing("phake", 31, '$file', $file);
 	
 	 Null;
 	if((not_dots($f) and is_dir($file))){
-		return compile_dir($file);
+		return compile_dir($file, $compile_func);
 	}
 	else if(is_phn($file)){
-		return (compile_file($file, dirname($file)) and print(("Compile: " . $file . "\n")));
+		return (is_string($compile_func)?$compile_func($file, dirname($file)):$compile_func[0]($file, dirname($file), $compile_func[1]) and print(("Compile: " . $file . "\n")));
 	}
 }
 
-function compile_dir($dir){
-	$__scope_id = Lexical::init_closure("phake", 28);
-	Lexical::bind_lexing("phake", 28, '$dir', $dir);
+function compile_dir($dir, $compile_func="compile_file"){
+	$__scope_id = Lexical::init_closure("phake", 30);
+	Lexical::bind_lexing("phake", 30, '$dir', $dir);
+	Lexical::bind_lexing("phake", 30, '$compile_func', $compile_func);
 
 
-	return map(array("phake__lambdafunc1", Lexical::get_closure_id("phake", $__scope_id)), scandir($dir, 1));
+	return map(array("phake__lambdafunc3", Lexical::get_closure_id("phake", $__scope_id)), scandir($dir, 1));
 }
 
 compile_with_flag("executable", project_path("/lib/phake/phake.phn"));
 compile_file(project_path("/phakefile"));
 require(project_path("/phakefile.php"));
 if((count($argv) > 1)){
-	$__tmpfuncname1 = $argv[1];
-	is_string($__tmpfuncname1)?$__tmpfuncname1():$__tmpfuncname1[0]($__tmpfuncname1[1]);
+	$__tmpfuncname0 = $argv[1];
+	is_string($__tmpfuncname0)?$__tmpfuncname0():$__tmpfuncname0[0]($__tmpfuncname0[1]);
 }
 else{
 	print("Doing nothing\n");
