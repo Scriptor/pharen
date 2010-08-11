e21e47bf53c738d8eb5ea525e4aa40add63a21f8
Tamreen <historium@gmail.com>
2010-08-10 22:59:43 -0400

    Bug fixes to the sql and html libraries.

--- lib/sql.php
+++ lib/sql.php
@@ -1,33 +1,38 @@
 <?php
 require_once('/Applications/MAMP/htdocs/pharen/lang.php');
 Lexical::$scopes['sql'] = array();
-
 function sql_connect($user, $pass, $db){
-
 	mysql_connect("localhost", $user, $pass);
 	return mysql_select_db($db);
 }
 
-function sql_vals($pairs){
+function sql_quote($v){
+	$val = mysql_real_escape_string($v);
+	if(is_string($val)){
+		return ("'" . $val . "'");
+	}
+	else{
+		return $val;
+	}
+}
 
-	return implode(", ", array_values($pairs));
+function sql_vals($pairs){
+	return implode(", ", map("sql_quote", array_values($pairs)));
 }
 
 function sql_cols($pairs){
-
-	return implode(", ", array_keys($paris));
+	return implode(", ", array_keys($pairs));
 }
 
 function sql_fetch_by_id($table, $id){
-	$__scope_id = Lexical::init_closure("sql", 47);
+	$__scope_id = Lexical::init_closure("sql", 29);
 	$query = sprintf("SELECT * FROM %s WHERE id=%s;", mysql_real_escape_string($table), mysql_real_escape_string($id));
-	Lexical::bind_lexing("sql", 47, '$query', $query);
+	Lexical::bind_lexing("sql", 29, '$query', $query);
 	return mysql_fetch_assoc(mysql_query($query));
 }
 
 function sql_insert($table, $pairs){
-
-	mysql_query(sprintf("INSERT INTO %s (%s) VALUES (%s);", mysql_real_escape_string($table), sql_cols($pairs), sql_vals($pairs)));
+	mysql_query(sprintf("INSERT INTO %s (%s) VALUES (%s);", mysql_real_escape_string($table), sql_cols($pairs), sql_vals($pairs))) or print(mysql_error());
 	return mysql_insert_id();
 }
 
