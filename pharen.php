a922f3f72202340da5096e318c32e7bcf2371a65
Tamreen <historium@gmail.com>
2010-09-13 03:01:31 -0400

    Don't run macros when ghosting.

--- pharen.php
+++ pharen.php
@@ -631,7 +631,7 @@ class Node implements Iterator, ArrayAccess, Countable{
         if(MicroNode::is_micro($func_name)){
             $micro = MicroNode::get_micro($func_name);
             return $micro->get_body(array_slice($this->children, 1), $this->indent);
-        }else if(MacroNode::is_macro($func_name)){
+        }else if(MacroNode::is_macro($func_name) && !MacroNode::$ghosting){
             $unevaluated_args = array_slice($this->children, 1);
             MacroNode::evaluate($func_name, $unevaluated_args);
 
