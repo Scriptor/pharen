63a5c13f667f1326f71f3f03a9d705709fe0c3e1
Scriptor <historium@gmail.com>
2011-07-10 12:45:19 -0400

    Use the correct path for lang.php.

--- lib/test.php
+++ lib/test.php
@@ -1,22 +1,22 @@
 <?php
-require_once('/Applications/MAMP/htdocs/pharen/lang.php');
+require_once('C:\wamp\www\pharen\lang.php');
 Lexical::$scopes['test'] = array();
 function check($expr, $expected){
-	$__scope_id = Lexical::init_closure("test", 43);
+	$__scope_id = Lexical::init_closure("test", 1);
 	if(($expr == $expected)){
 		return TRUE;
 	}
 	else{
 		$bt = debug_backtrace();
-	Lexical::bind_lexing("test", 43, '$bt', $bt);
-		error_log(("Test failed on line: " . $bt[0]["line"] . " in " . $bt[0]["file"]));
+	Lexical::bind_lexing("test", 1, '$bt', $bt);
+		error("Test failed on line: ", $bt[0]["line"], " in ", $bt[0]["file"]);
 		return FALSE;
 	}
 }
 
 function describe($msg, $func){
 	echo(("Running tests for: " . $msg . "\n"));
-	return is_string($func)?$func():$func[0]($func[1]);
+	return (is_string($func)?$func():$func[0]($func[1]));
 }
 
 function it($msg, $func){
