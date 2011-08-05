27ea95c2b0d80ee8ca03107205d30b60ddea89aa
Scriptor <historium@gmail.com>
2011-08-05 12:40:09 -0400

    Convert * to __star in names.

--- pharen.php
+++ pharen.php
@@ -919,7 +919,8 @@ class LeafNode extends Node{
         $char_mappings = array(
             '-'=>'_',
             '?'=>'__question',
-            '!'=>'__exclam'
+            '!'=>'__exclam',
+            '*'=>'__star'
         );
         foreach($char_mappings as $char=>$replacement){
             $name = str_replace($char, $replacement, $name);
