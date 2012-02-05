7fc8b862f48fae06b75714a4bc1ee0b7f6ecd758
ken restivo <ken@restivo.org>
2012-02-05 11:22:55 -0800

    play nice with distributions, put stuff in /usr/local/bin
    Signed-off-by: Tamreen Khan <historium@gmail.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pharen_dir=$(dirname $(readlink -f $0))
-if [ ! -e "/usr/bin/pharen" ]
+if [ ! -e "/usr/local/bin/pharen" ]
 then
-    ln -s $pharen_dir/bin/pharen /usr/bin/pharen
+    ln -s $pharen_dir/bin/pharen /usr/local/bin/pharen
     echo "Pharen successfully installed."
 else
-    echo "There is already a file called 'pharen' in /usr/bin. Maybe it's already installed?"
+    echo "There is already a file called 'pharen' in /usr/local/bin. Maybe it's already installed?"
 fi
