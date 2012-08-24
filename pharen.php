2285cd26a3cad5534906c2253c88b0fd3890ad33
Scriptor <historium@gmail.com>
2012-08-24 16:42:16 -0400

    Put the namespace's name inside the quotes in a partial's string representation.

--- pharen.php
+++ pharen.php
@@ -709,7 +709,7 @@ class Node implements Iterator, ArrayAccess, Countable{
     public function create_partial($func){
         list($tmp_func, $tmp_name) = $func->get_tmp_func($this->parent);
         Node::$tmpfunc .= $tmp_func;
-        return RootNode::$ns.'"\\\\'.$tmp_name.'"';
+        return '"'.RootNode::$ns.'\\\\'.$tmp_name.'"';
     }
 
     public function compile($is_statement=False, $is_return=False){
